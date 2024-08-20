de78c4310d1fe2eab227a59d160c42c701eed5e9
Dana-Farber <Dana-Farber@users.noreply.github.com>
2024-08-20 17:12:08 -0400

    Update demo.py w/ embedding models, 4o mini offerings

--- demo.py
+++ demo.py
@@ -3,8 +3,9 @@ import os
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from openai import AzureOpenAI
 
-# deployment name
+# Completion example, pick a model
 # model = "gpt-4-turbo-0125-api" # this is GPT-4 Turbo 0125-preview
+# model = "gpt-4o-mini-2024-07-18-api" # this is GPT-4o mini
 model = "gpt-4o-2024-05-13-api" # this is GPT-4o 2024-05-13
 api_version = "2023-05-15"
 
@@ -31,3 +32,31 @@ response = client.chat.completions.create(
 )
 
 print(response)
+
+# Embedding example, pick a model
+# emodel = "text-embedding-3-small-1-api" # this is Azure OpenAI embedding small v1
+embedding_model = "text-embedding-3-large-1-api" # this is the Azure OpenAI flagship embedding (large) v1 
+api_version = "2023-05-15"
+
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+entra_scope = os.getenv("AZURE_OPENAI_ENTRA_SCOPE")
+
+token_provider = get_bearer_token_provider(
+    DefaultAzureCredential(), 
+    entra_scope
+)
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=endpoint,
+    azure_ad_token_provider=token_provider,
+)
+
+response = client.embeddings.create(
+    model=embedding_model,
+    input = "Hello, world!"
+)
+
+print(response.model_dump_json(indent=2))
+
+
